f1411211050d01404f2d487823cd7f1017e26f77
Fedekh <federicocet@gmail.com>
2023-06-06 21:02:33 +0200

    fix comic with project

--- resources/views/admin/projects/delete.blade.php
+++ resources/views/admin/projects/delete.blade.php
@@ -1,6 +1,6 @@
 <div class="hider">
     <div id="confirmDelete" class="p-4">
-        <p>Sei sicuro di voler eliminare questo fumetto?</p>
+        <p>Sei sicuro di voler eliminare questo progetto?</p>
         <button id="confirmBtn" class="border-0 text-white p-2 mx-4 rounded">Conferma</button>
         <button id="cancelBtn" class="border-0 text-white  p-2 rounded">Annulla</button>
     </div>
